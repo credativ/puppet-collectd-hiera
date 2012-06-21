24ee0fb8cc15e6938b4ede69c9804060bce3d66c
Alexander Wirt <alexander.wirt@credativ.de>
2012-06-21 13:00:10 +0200

    Remove debugmessage

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,8 +1,6 @@
 class collectd::client inherits collectd {
 	$server = params_lookup('osf_mng_server_ip', 'global')
 
-	notify{"The value is: ${server}": }
-
 	plugin { 'network':
 		options => "\tServer \"${server}\""
 	}
