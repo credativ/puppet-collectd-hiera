3ea8c3d72d5d6e9e928bee9da0ee9ee420355330
Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
2012-06-22 14:33:47 +0200

    Add missing params class

---
+++ manifests/params.pp
@@ -0,0 +1,8 @@
+class collectd::params {
+    $listener   = undef
+    $server     = undef
+    $ensure         = present
+    $ensure_running = true
+    $ensure_enabled = true
+    $disabled_hosts = []
+}
