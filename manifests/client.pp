c09dfff42c0e61eba4a888b89569ad39ebb52824
Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
2012-08-17 11:12:07 +0200

    Add docs and missing params

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,9 +1,47 @@
-class collectd::client (
-        $ensure         = params_lookup('ensure'),
-        $ensure_running = params_lookup('ensure_running'),
-        $ensure_enabled = params_lookup('ensure_enabled'),
-        $server         = params_lookup('server'),
-    ) inherits collectd {
+# = Class: collectd
+#
+# Module to manage collectd
+#
+# == Requirements:
+#
+# - This module makes use of the example42 functions in the puppi module
+#   (https://github.com/credativ/puppet-example42lib)
+#
+# == Parameters:
+#
+# [*ensure*]
+#   What state to ensure for the package. Accepts the same values
+#   as the parameter of the same name for a package type.
+#   Default: present
+#   
+# [*ensure_running*]
+#   Weither to ensure running collectd or not.
+#   Default: running
+#
+# [*ensure_enabled*]
+#   Weither to ensure that collectd is started on boot or not.
+#   Default: true
+#
+# [*disabled_hosts*]
+#   A list of hosts whose collectd will be disabled, if their
+#   hostname matches a name in the list.
+#
+# [*server*]
+#   Define the hostname/ip of the collectd server
+#   Default: undef
+# == Author:
+#
+#   Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
+class collectd (
+    $ensure             = params_lookup('ensure'),
+    $manage_config      = params_lookup('manage_config'),
+    $ensure_running     = params_lookup('ensure_running'),
+    $ensure_enabled     = params_lookup('ensure_enabled'),
+    $config_source      = params_lookup('config_source'),
+    $config_template    = params_lookup('config_template'),
+    $disabled_hosts     = params_lookup('disabled_hosts')
+    $server             = params_lookup('server'),
+    ) inherits collectd::params {
 
     plugin { 'network':
         options => "\tServer \"${server}\""
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,41 @@
+# = Class: collectd
+#
+# Module to manage collectd
+#
+# == Requirements:
+#
+# - This module makes use of the example42 functions in the puppi module
+#   (https://github.com/credativ/puppet-example42lib)
+#
+# == Parameters:
+#
+# [*ensure*]
+#   What state to ensure for the package. Accepts the same values
+#   as the parameter of the same name for a package type.
+#   Default: present
+#   
+# [*ensure_running*]
+#   Weither to ensure running collectd or not.
+#   Default: running
+#
+# [*ensure_enabled*]
+#   Weither to ensure that collectd is started on boot or not.
+#   Default: true
+#
+# [*disabled_hosts*]
+#   A list of hosts whose collectd will be disabled, if their
+#   hostname matches a name in the list.
+#
+# == Author:
+#
+#   Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
 class collectd (
+    $ensure             = params_lookup('ensure'),
+    $manage_config      = params_lookup('manage_config'),
+    $ensure_running     = params_lookup('ensure_running'),
+    $ensure_enabled     = params_lookup('ensure_enabled'),
+    $config_source      = params_lookup('config_source'),
+    $config_template    = params_lookup('config_template'),
     $disabled_hosts = params_lookup('disabled_hosts')
   ) inherits collectd::params {
 
